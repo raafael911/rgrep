1cfb31d8e286e4749c5fc73ef7678d5509a33c20
Kevin Neuenfeldt <kevin.neuenfeldt@posteo.de>
2017-11-24 12:29:47 +0100

    Refactoring: moved logic into lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,89 @@
+extern crate clap;
+extern crate regex;
+extern crate colored;
+
+use colored::*;
+use std::io::prelude::*;
+use std::fs::File;
+use clap::{Arg, App};
+use regex::{Regex, Match};
+
+
+/// Struct containing configuration for the search algorithm
+pub struct Config {
+    file_content: String,
+    regex: Regex
+}
+
+
+impl Config {
+    pub fn new() -> Config {
+
+        let cmd_params = App::new("My Super own grep program")
+                        .version("1.0")
+                        .author("Kevin Neuenfeldt <kevin.neuenfeldt@posteo.de>")
+                        .about("Does awesome things")
+                        .arg(Arg::with_name("PATTERN")
+                            .help("A Pattern to look for")
+                            .required(true)
+                            .index(1))
+                        .arg(Arg::with_name("FILE")
+                            .help("A file to search for PATTERN")
+                            .required(true)
+                            .index(2))
+                        .get_matches();
+
+        let pattern = cmd_params.value_of("PATTERN").unwrap();
+        let file_path = cmd_params.value_of("FILE").unwrap();
+
+        let mut file = match File::open(file_path) {
+            Err(_) => panic!(),
+            Ok(f) => f
+        };
+
+        let regex = match Regex::new(pattern) {
+            Err(_) => panic!(),
+            Ok(r) => r
+        };
+
+        let mut file_content = String::new();
+
+        match file.read_to_string(&mut file_content) {
+            Err(_) => panic!(),
+            Ok(_) => ()
+        };
+
+        Config {
+            file_content: file_content,
+            regex: regex
+        }
+    }
+}
+
+
+/// Implementation of the search algorithm
+pub fn do_search(params: &Config) {
+
+    for (line_number, line) in params.file_content.lines().enumerate() {
+
+        for m in params.regex.find_iter(&line[..]) {
+
+            print_match(&line, line_number, &m);
+        }
+    }
+}
+
+
+/// Prints a line describing a match
+fn print_match(line: &str, line_number: usize, m: &Match) {
+
+    // TODO do not print a new line for each match in a line
+    let pref = String::from(&line[0..m.start()]);
+    let inf = String::from(m.as_str());
+    let postf = String::from(&line[m.end()..]);
+
+    print!("{}: ", line_number.to_string().cyan());
+    print!("{}", pref);
+    print!("{}", inf.bright_red());
+    println!("{}", postf);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,89 +1,10 @@
-extern crate clap;
-extern crate regex;
-extern crate colored;
+extern crate rgrep;
 
-use colored::*;
-use std::io::prelude::*;
-use std::fs::File;
-use clap::{Arg, App};
-use regex::{Regex, Match};
-
-struct CmdParams {
-    file_content: String,
-    regex: Regex
-}
-
+use rgrep::Config;
 
 fn main() {
 
-    let params = parse_params();
+    let params = Config::new();
 
-    do_search(&params);
-}
-
-fn do_search(params: &CmdParams) {
-
-    for (line_number, line) in params.file_content.lines().enumerate() {
-
-        for m in params.regex.find_iter(&line[..]) {
-
-            print_match(&line, line_number, &m);
-        }
-    }
-}
-
-fn parse_params() -> CmdParams {
-
-    let cmd_params = App::new("My Super own grep program")
-                    .version("1.0")
-                    .author("Kevin Neuenfeldt <kevin.neuenfeldt@posteo.de>")
-                    .about("Does awesome things")
-                    .arg(Arg::with_name("PATTERN")
-                        .help("A Pattern to look for")
-                        .required(true)
-                        .index(1))
-                    .arg(Arg::with_name("FILE")
-                        .help("A file to search for PATTERN")
-                        .required(true)
-                        .index(2))
-                    .get_matches();
-
-    let pattern = cmd_params.value_of("PATTERN").unwrap();
-    let file_path = cmd_params.value_of("FILE").unwrap();
-
-    let mut file = match File::open(file_path) {
-        Err(_) => panic!(),
-        Ok(f) => f
-    };
-
-    let regex = match Regex::new(pattern) {
-        Err(_) => panic!(),
-        Ok(r) => r
-    };
-
-    let mut file_content = String::new();
-
-    match file.read_to_string(&mut file_content) {
-        Err(_) => panic!(),
-        Ok(_) => ()
-    };
-
-    CmdParams {
-        file_content: file_content,
-        regex: regex
-    }
-}
-
-/// Printsa line describing a match
-fn print_match(line: &str, line_number: usize, m: &Match) {
-
-    // TODO do not print a new line for each match in a line
-    let pref = String::from(&line[0..m.start()]);
-    let inf = String::from(m.as_str());
-    let postf = String::from(&line[m.end()..]);
-
-    print!("{}: ", line_number.to_string().cyan());
-    print!("{}", pref);
-    print!("{}", inf.bright_red());
-    println!("{}", postf);
+    rgrep::do_search(&params);
 }
